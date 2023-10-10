a24ac01d2181edfa39a28592bf10483e7fba9829
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-10-10 16:07:03 -0500

    Initial JS work

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var dayDisplay =('cuurentDay')
+var scheduleLocation = ('container-lg');
+var clickSave = ('.saveBtn')
+
+const timeStart = 9;
+const hoursBlock = 9;
+
+var now = dayjs();
+var currentTime = now.hour();
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +27,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
 });
+
+displayTime();
+setInterval(displayTime, 1000);
