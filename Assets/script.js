13ecab49523387bd13beff0bf523c9d3e27a9e59
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-10-11 15:52:57 -0500

    changes after tutor

--- Assets/script.js
+++ Assets/script.js
@@ -1,48 +1,64 @@
-var scheduleLocation = ('container-lg');
-var dayDisplay = ('cuurentDay')
-var clickSave = ('.saveBtn')
+var scheduleLocation = $(".container-lg");
+var clickSave = $('.saveBtn')
+var dayDisplay = $("#currentDay")
+
 const hoursBlockTotal = 9;
 const timeStart = 9;
 var now = dayjs();
 var currentTime = now.hour();
 
-function pageBuild() {
-  for (i = 0; i < hoursBlockTotal; i++){
+function buildPage(){
+  for(i = 0; i < hoursBlockTotal; i++){
     var timeID = i+timeStart
     var timeSlot = i+timeStart
     var timeSlotSuf = "AM"
     var notifyColor = ""
-    if (currentTime > timeID)notifyColor = "past"
+    if (currentTime > timeID)notifyColor = "past";
     else if (currentTime === timeID)notifyColor="present";
     else notifyColor="future";
     if (timeID === 0) timeSlot = 12;
-    else if (timeId > 12 && timeID < 24){
-      timeSlot -= 12;
+    else if (timeID > 12 && timeID < 24){
+      timeSlot =-12;
       timeSlotSuf = "PM";
     }
     var blockLoadID = (timeSlot+timeSlotSuf);
     var blockLoadText = JSON.parse(localStorage.getItem(blockLoadID));
     if (blockLoadText === null) blockLoadText = "";
-    if (timeId < 24){
-      var row = `<div id="hour-${timeID}" class="row time-block ${notifyColor}">
+    if (timeID < 24){
+      var row = `<div id="hour-${timeID}" class="row time-block">
     <div class="col-2 col-md-1 hour text-center py-3">${timeSlot}${timeSlotSuf}</div>
-    <textarea class="col-8 col-md-10 description" rows="3">${blockLoadText} </textarea>
+    <textarea class="col-8 col-md-10 description ${notifyColor} " rows="3">${blockLoadText} </textarea>
     <button class="btn saveBtn col-2 col-md-1" aria-label="save">
       <i class="fas fa-save" aria-hidden="true"></i>
     </button>
   </div>
   `
-      $(scheduleLocation).append(row);
+      scheduleLocation.append(row);
+      getText()
     }
   }
 }
+  function getText(){
+$(".description").each(function(){
+  console.log($(this))
+})
+  }
 
-$(scheduleLocation).on("click", clickSave, function(event){
-  var blockSaveText = $(this).siblings("textarea").val();
-  var blockSaveID = $(this).siblings(".hour").text();
+scheduleLocation.on("click", function(event){
+  console.log(event.target.className)
+  if (event.target.className == "btn saveBtn col-2 col-md-1"){
+
+ 
+  var blockSaveID = event.target.parentElement.id
+  // var blockSaveID = $(this).siblings(".hour").text();
+  var blockSaveText = event.target.previousElementSibling.value
+  console.log(blockSaveText, blockSaveID);
+  console.log("text");
   localStorage.setItem(blockSaveID, JSON.stringify(blockSaveText));
+}
 })
 
-$(dayDisplay).text(now.format("dddd, MMMM DD"));
+
+dayDisplay.text(now.format("dddd, MMMM DD"));
 buildPage();
 
